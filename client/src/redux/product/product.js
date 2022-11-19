e8a66847e81b37e13e7478679d48e6eb6886fe7c
moise <moisemlg90@gmail.com>
2022-11-19 16:26:20 +0200

    Add reducer for a single product Create a product folder inside the reducer Create a product file Create a product reducer Fetch data from the graphql end point Add data from a new field list export the default product reducer

---
+++ client/src/redux/product/product.js
@@ -0,0 +1,45 @@
+import { client, Query, Field } from "@tilework/opus";
+
+const UPDATE = "getProducts";
+
+const initialState = [];
+
+export const GetProductsForAllCategories = () => async (dispatch) => {
+  try {
+    client.setEndpoint("http://localhost:4000/graphql");
+
+    const categoryQuery = new Query("product", true);
+      .addArgument("input", "String", { title: "all" });
+      .addFieldList(["id", "name", "InStock", "gallery"]);
+
+      const queryResult = await client.post(categoryQuery);
+      console.log("GetProductsForAllCategories() fetchedData", queryResult);
+
+    dispatch({ type: UPDATE, payload: queryResult });
+    } catch (e) {
+    dispatch({ type: UPDATE, payload: [] });
+    console.log("error", e);
+    }
+
+};
+
+const productReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case UPDATE: {
+
+            const product = action.payload?.category?.product?.id;
+            console.log('productReducer() product', {
+              everything: action.payload,
+              product,
+            });
+      
+            return product;
+          }
+          default:
+            return state;
+        }
+      };
+      
+      export default productReducer;
+      
+
