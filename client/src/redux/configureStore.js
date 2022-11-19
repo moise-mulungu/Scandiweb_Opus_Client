86b0e62a2ee3eb6ef3b28a2bcdc406b65f2c1331
moise <moisemlg90@gmail.com>
2022-11-19 16:38:02 +0200

    Import product reducer to the config store file

--- client/src/redux/configureStore.js
+++ client/src/redux/configureStore.js
@@ -3,11 +3,13 @@ import thunk from 'redux-thunk';
 import booksReducer from './books/books';
 import productsReducer from './products/products';
 import categoriesReducer from './categories/categories';
+import productReducer from './product/product';
 
 const reducer = combineReducers({
   booksReducer,
   categoriesReducer,
   productsReducer,
+  productReducer,
 });
 
 const configureStore = createStore(reducer, applyMiddleware(thunk));
